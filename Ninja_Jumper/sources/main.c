08e1e101f4723ba357cc026b9a302de6ed6facef
Ravenel Pierre <31650142+pierreravenel@users.noreply.github.com>
2018-02-17 19:43:56 +0100

    Title

--- Ninja_Jumper/sources/main.c
+++ Ninja_Jumper/sources/main.c
@@ -27,7 +27,32 @@ HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABI
 OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS 
 SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-*/
+
+ _   _ ___ _   _     _   _          _ _   _ __  __ ____  _____ ____  
+| \ | |_ _| \ | |   | | / \        | | | | |  \/  |  _ \| ____|  _ \ 
+|  \| || ||  \| |_  | |/ _ \    _  | | | | | |\/| | |_) |  _| | |_) |
+| |\  || || |\  | |_| / ___ \  | |_| | |_| | |  | |  __/| |___|  _ < 
+|_| \_|___|_| \_|\___/_/   \_\  \___/ \___/|_|  |_|_|   |_____|_| \_\
+                                                                     
+ * 
+ * 	Projet ISN 2017 : Ravenel Pierre
+ *  
+ *  But du jeu: Ninja jumper est un jeu de plateforme ( try and retry )
+ *  le personnage principal qui est un ninja devra parcourir des obstacles
+ *  afin d'arriver Ã  la sortie.
+ * 
+ *  Commandes: 
+ * 		-> délacements (flèches)
+ * 		-> quitter (échap)
+ *   -> en cas de bugs (f1)
+ *  
+ * 	/!\ #define rafraichissement 10 /!\ variable qui modifie le temps entre
+ *   chaque rafraichissement
+ * 	(A modifié en fonction de la rapidité ou lenteur du jeu)
+ * 	
+ * 	GL HF !
+ *
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
